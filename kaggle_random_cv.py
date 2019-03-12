fa44e2f3bba88ee45b11511adf77d39abe5cf75e
kokoichiro <Koichiro.Kondo@dentsuaegis.com>
2019-03-12 14:29:23 +0000

    amend data pipeline and configuration.

--- kaggle_random_cv.py
+++ kaggle_random_cv.py
@@ -1,3 +1,4 @@
+
 import pandas as pd
 import numpy as np
 from sklearn.tree import DecisionTreeClassifier
@@ -10,20 +11,30 @@ from sklearn.model_selection import train_test_split,GridSearchCV,RandomizedSear
 from sklearn.decomposition import PCA
 from sklearn.ensemble import (RandomForestClassifier,RandomForestRegressor,GradientBoostingClassifier,GradientBoostingRegressor,BaggingClassifier,VotingClassifier,AdaBoostClassifier)
 from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from sklearn.neural_network import MLPClassifier
 from sklearn.externals.six import StringIO
 #from imblearn.over_sampling import ADASYN, SMOTE, RandomOverSampler
 #from imblearn.pipeline import make_pipeline
 from sklearn.preprocessing import MinMaxScaler,LabelEncoder,StandardScaler
 from sklearn.metrics import (brier_score_loss, precision_score, recall_score,roc_auc_score,
- f1_score,accuracy_score,confusion_matrix,mean_absolute_error,r2_score,mean_squared_error)
+                             f1_score,accuracy_score,confusion_matrix,mean_absolute_error,r2_score,mean_squared_error)
 import datetime
 
 import time
 import os
 import sys
 
-import argparse
+from scipy import misc
+from scipy.stats import randint
 
+import yaml
+import time
+import os
+import sys
+
+import argparse
+from random import randint
 
 class kaggleexploration():
 
@@ -51,7 +62,7 @@ class kaggleexploration():
 		n_jobs=0,
 		record_txt=None,
 		id=None):
-		with open(yml_loc,'r') as ymlfile:
+		with open(config_loc,'r') as ymlfile:
 			cfg = yaml.load(ymlfile)
 
 		self.training_file=cfg['file']['training_file']
@@ -60,7 +71,7 @@ class kaggleexploration():
 		self.explore_test_rate=cfg['ML_parameter']['explore_test_rate']
 		self.target_name=cfg['ML_parameter']['target_name']
 		self.target_metric=cfg['ML_parameter']['target_metric']
-		self.training_rate=cfg['ML_parameter']['target_rate']
+		self.training_rate=cfg['ML_parameter']['training_rate']
 		self.n_jobs=cfg['ML_parameter']['n_jobs']
 		self.record_txt=cfg['file']['record_txt']
 		self.id=cfg['ML_parameter']['id']
@@ -69,7 +80,7 @@ class kaggleexploration():
 	def get_score(clf, train_features, train_labels):
 		X_train, X_test, y_train, y_test = train_test_split(train_features, train_labels, test_size=0.5, random_state=0)
 		clf.fit(X_train, y_train)
-		print clf.score(X_test, y_test)
+		print (clf.score(X_test, y_test))
 
 	def get_accuracy(clf, train_features, train_labels):
 		scores = cross_validation.cross_val_score(clf, train_features, train_labels, cv=10)
@@ -79,8 +90,8 @@ class kaggleexploration():
 	def random_searchSVM(self,train_features, train_labels, n_jobs,objective):
 		param_dist = {
 	#   {'C': [1, 10, 100], 'kernel': ['linear']},
-						'C': [1,10,50,100], 'gamma': [0.0001, 0.001], 'kernel': ['rbf','linear']}
-	#  {'C': [1], 'gamma': [0.001], 'kernel': ['rbf']}
+						#'C': [1,10,50,100], 'gamma': [0.0001, 0.001], 'kernel': ['rbf','linear']}
+	  	'C': [1], 'gamma': [0.001], 'kernel': ['rbf']}
 	
 
 		clf = GridSearchCV(svm.SVC(class_weight="balanced"), param_dist,n_jobs=n_jobs,scoring=objective)
@@ -102,12 +113,12 @@ class kaggleexploration():
 			'n_estimators'  : randint(5, 300),
 			'max_features'  : randint(3, 20),
 			'min_samples_split' : randint(3, 100),
-			'max_depth' : randint(3, 100)}
+			'max_depth' : randint(4, 30)}
 		clf = RandomizedSearchCV(RandomForestClassifier(class_weight="balanced"), param_dist,n_jobs=n_jobs,scoring=objective)
 		clf.fit(train_features, train_labels)
 		return clf.best_estimator_,clf.best_score_,clf.best_params_
 
-	def random_searchVC(train_features,train_labels,n_jobs,objective):
+	def random_searchVC(self,train_features,train_labels,n_jobs,objective):
 		clf1 = LogisticRegression(random_state=1,class_weight="balanced")
 		clf2 = RandomForestClassifier(random_state=1,class_weight="balanced")
 		clf3 = GaussianNB()
@@ -117,7 +128,7 @@ class kaggleexploration():
 		clf.fit(train_features, train_labels)
 		return clf.best_estimator_,clf.best_score_,clf.best_params_
 
-	def random_searchAB(train_features,train_labels,n_jobs,objective):
+	def random_searchAB(self,train_features,train_labels,n_jobs,objective):
 		param_dist = {
 		'algorithm'  : ['SAMME', 'SAMME.R'],
 		'n_estimators'  : randint(5, 300)}
@@ -127,7 +138,7 @@ class kaggleexploration():
 		return clf.best_estimator_,clf.best_score_,clf.best_params_
 
 
-	def random_searchNC(train_features,train_labels,n_jobs,objective):
+	def random_searchNC(self,train_features,train_labels,n_jobs,objective):
 		param_dist = {
 		'activation'  : ['identity', 'logistic', 'tanh', 'relu'],
 		'hidden_layer_sizes' : [(5,2), (10,5),(10,20,5),(5,10,10,5)],
@@ -259,7 +270,7 @@ class kaggleexploration():
 			VC_score=roc_auc_score(test_labels, VC_y_pred)
 			NC_score=roc_auc_score(test_labels, NC_y_pred)
 			AB_score=roc_auc_score(test_labels, AB_y_pred)
-		print 'RF:'+str(RF_score)+', GB:'+str(GB_score)+', SVM:'+str(SVM_score)+', VC:'+str(VC_score)+', AB:'+str(AB_score)+', NC:'+str(NC_score)
+		print ('RF:'+str(RF_score)+', GB:'+str(GB_score)+', SVM:'+str(SVM_score)+', VC:'+str(VC_score)+', AB:'+str(AB_score)+', NC:'+str(NC_score))
 		score_list = [RF_score,GB_score,SVM_score,VC_score,NC_score,AB_score]
 		if RF_score ==max(score_list):
 			return a_RF,b_RF,c_RF
@@ -285,7 +296,7 @@ class kaggleexploration():
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(prog='kaggle_random_cv.py',usage='Use this code for exploring the best methodology and parameter for ML.',description='this code needs 2 different args')
 	parser.add_argument('configuration', help = 'The location of Configuration file')
-	parser.add_argument('--mode',required=False,help = 'You can select one variable from 3 parameters you can skip this parameter.',default='all',choices=['all','random_search','training'])
+	parser.add_argument('--mode',required=False,help = 'You can select one variable from 3 parameters you can skip this parameter.',default='all',choices=['all','random_search','simple_ml'])
 
 	args=parser.parse_args()
 
@@ -298,22 +309,31 @@ if __name__ == "__main__":
 	train_features1=df_train[exp.target_name]
 	train_labels1=df_train.drop(exp.id,axis=1)
 	train_labels1=train_labels1.drop(exp.target_name,axis=1)
+	if args.mode == 'all' or 'random_search':
+		y_train, y_test, X_train, X_test = train_test_split(train_features1, train_labels1,test_size=exp.explore_test_rate, random_state=0)
+		a,b,c=exp.best_estimater(X_train, y_train,X_test,y_test,exp.target_metric)
+		a_txt = str(a)
 
-	y_train, y_test, X_train, X_test = train_test_split(train_features1, train_labels1,test_size=exp.explore_test_rate, random_state=0)
-	a,b,c=exp.best_estimater(X_train, y_train,X_test,y_test,exp.target_metric)
-	a_txt = str(a)
+		with  open(exp.record_txt,mode='w') as f:
+			f.write(a_text)
+			f.write(b)
+			f.write(c)
+	if args.mode == 'all' or 'simple_ml':
 
-	with  open(exp.record_txt,mode='w') as f:
-		f.write(a_text)
-		f.write(b)
-		f.write(c)
-
-	ML=a
-	ML.fit(train_labels1,train_features1)
-	#ML.fit(X_train, y_train)
-	df_test=pd.read_csv(exp.test_file)
-	df_label_test=df_test.drop(exp.id,axis=1)
-	y_pred=ML.predict(df_label_test)
-	y_pred = ML.predict(X_test)
-	df_answer=pd.concat([df_test[exp.id],y_pred],axis=1)
-	df_answer.to_csv(exp.submission_file)
+		if args.mode == 'all':
+			ML=a
+		elif args.mode == 'simple_ml':
+			ML=VotingClassifier(estimators=[('lr', LogisticRegression(C=199, class_weight='balanced', dual=False,
+			fit_intercept=True, intercept_scaling=1, max_iter=100,
+			multi_class='ovr', n_jobs=1, penalty='l2', random_state=1,
+			solver='liblinear', tol=0.0001, verbose=0, warm_start=False)), ('rf', Rand...=False, random_state=1,
+			verbose=0, warm_start=False)), ('gnb', GaussianNB(priors=None))],
+			flatten_transform=None, n_jobs=1, voting='soft', weights=None)
+		ML.fit(train_labels1,train_features1)
+		#ML.fit(X_train, y_train)
+		df_test=pd.read_csv(exp.test_file)
+		df_label_test=df_test.drop(exp.id,axis=1)
+		y_pred=ML.predict(df_label_test)
+		y_pred = ML.predict(X_test)
+		df_answer=pd.concat([df_test[exp.id],y_pred],axis=1)
+		df_answer.to_csv(exp.submission_file,index=False)
